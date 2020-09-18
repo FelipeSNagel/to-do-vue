f37f92a6a7e69bac77d78ad92b5dd9985b7a3677
Felipe S Nagel <felipesnagel@gmail.com>
2020-09-18 11:17:17 -0300

    conclusão do projeto básico

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container grid-xs py-2">
+      <h1 class="title flex-centered">To-<span>Do</span> List <i class="icon icon-check"></i></h1>
+
+      <form @submit.prevent="addToDo(toDo)">
+        <div class="input-group">
+          <input v-model="toDo.description" type="text" class="form-input" placeholder="Novo toDo"/>
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <ul class="todo-list">
+        <ToDoItem v-for="t in toDos" :key="t.id" :toDo="t"
+          @toggle="toggleToDo" @remove="removeToDo"
+         />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import ToDoItem from './components/ToDoItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ToDoItem,
   },
+  data() {
+    return {
+      toDos: [],
+      toDo: {
+        checked: false,
+      },
+    };
+  },
+  methods: {
+    addToDo(toDo) {
+      this.toDos.push({
+        ...toDo,
+        id: Date.now(),
+      });
+
+      this.toDo = { checked: false };
+    },
+
+    toggleToDo(param) {
+      const index = this.toDos.findIndex((item) => item.id === param.id);
+
+      if (index > -1) {
+        const checked = !this.toDos[index].checked;
+        this.$set(this.toDos, index, { ...this.toDos[index], checked });
+      }
+    },
+
+    removeToDo(param) {
+      const index = this.toDos.findIndex((item) => item.id === param.id);
+
+      if (index > -1) {
+        this.$delete(this.toDos, index);
+      }
+    },
+
+  },
+
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .title {
+    font-size: 25px;
+    font-weight: 700;
+    color: #3b3b3b;
+    margin: 30px auto;
+  }
+
+  .title span {
+    color: #5755d9;
+  }
+
+  .title i {
+    color: #5755d9;
+    margin-left: 0.5rem;
+  }
+
+  .todo-list {
+    padding-top: 2rem;
+  }
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="tile" :class="{checked: toDo.checked}">
+    <div class="tile-icon">
+      <i class="icon centered" :class="toDo.checked ? 'icon-check' : 'icon-time'"></i>
+    </div>
+
+    <div class="tile-content">
+      <p class="title-subtitle">{{ toDo.description }}</p>
+    </div>
+
+    <div class="title-action">
+      <button @click="$emit('toggle', toDo)" class="btn btn-link">
+        <span v-if="toDo.checked">Desmarcar</span>
+        <span v-else>Concluído</span>
+      </button>
+      <button @click="$emit('remove', toDo)" class="btn btn-link text-error">Remover</button>
+    </div>
+  </li>
+</template>
+
+<script>
+
+export default {
+  props: {
+    toDo: { type: Object, required: true },
+  },
+};
+
+</script>
+
+<style scoped>
+  .checked {
+    text-decoration: line-through;
+    color: lightgray;
+  }
+</style>
